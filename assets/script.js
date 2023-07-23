8b50ed91c0c80e7407dbbca6f84f056a4b27a684
Vilson Apostolovski II <vilsonapo@gmail.com>
2023-07-23 14:58:07 -0400

    Added fetch requests to convert City to Lat/Lon and retrive weather data

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,15 @@ let todaysForecastEl = document.querySelector("#oneDay");
 let todaysForecastTitle = document.querySelector("#cityName");
 let todaysForecastData = document.querySelector("#cityDetails");
 
+const openWeatherAPI = "e1c07b6ea350045571a9f63ab10e6b58"
+
+
 let formSubmitHandler = function(event) {
     event.preventDefault();
     let city = searchInputEl.value.trim();
     if (city) {
         console.log("User input was: " + city);
+        getCoordinates(city)
     } else {
         alert("Please enter a city")
     }
@@ -23,7 +27,33 @@ let formSubmitHandler = function(event) {
 searchInputForm.addEventListener("submit", formSubmitHandler);
 
 // Function getCoordinates= Input: "City", output: (lat, long)
+function getCoordinates(city) {
+    let geocodingURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${openWeatherAPI}`;
+
+    fetch(geocodingURL)
+        .then(response => response.json())
+            .then(data => {
+                if (data[0]){
+                    getWeatherData(data[0].lat, data[0].lon);
+                } else {
+                    alert("City not found.");
+                }
+            })
+        .catch(error => console.log('Error: ', error))
+}
+
 // Function getWeatherData= Input: (Lat, Log), Output: Save to local storage function
+function getWeatherData(lat, lon) {
+    let weatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${openWeatherAPI}`;
+    fetch(weatherURL)
+        .then(response => response.json())
+            .then(data => {
+                console.log("Data: ");
+                console.log(data);
+                // Temperature in Kelvins? 
+                // Convert to F
+            })
+}
 // Function saveToLocal= Input: {Weather Data}, city, Append to cities set cities to local storage
 // Function displayResults= Write to HTML from local storage 
 
